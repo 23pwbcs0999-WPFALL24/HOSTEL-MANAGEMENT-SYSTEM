03c84399ef1367931a40e16ca93b5dfa9188cef0
23pwbcs0999-WPFALL24 <23pwbcs0999@uetpeshawar.edu.pk>
2025-08-13 12:40:43 +0500

    Commit

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -68,6 +68,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, 
     dialect: 'mysql',
     logging: (msg) => {
       console.log(msg);
